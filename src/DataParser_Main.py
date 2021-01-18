435f2b4dae11f64386156012bb5f74cb45c5516e
tolufive <tolufive@gmail.com>
2021-01-17 18:19:16 -0700

    tolu 6:19

---
+++ src/DataParser_Main.py
@@ -0,0 +1,19 @@
+from DataParser import Database
+
+
+class DataParserMain:
+    TOKEN = "GBQF4QKrQcQzWwX2R5mmGJ"
+    REST_KEY = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhYmFzZUlkIjoiU0d4SHc2ZWZrdkxWdWhUakFzbzhVQyIsImFjY2Vzc1Blcm0iOiJmdWxsIiwidG9rZW5JZCI6IlZCUHdlaDdCV09yMlhJRTcxbkxsNWJrT1l6b05TdkN6Q1pUYjhIdzJrQ3psWjBST3BwOGJYd3pRdUJteFJBR3EiLCJpYXQiOjE2MTA5Mjc2NTcsImV4cCI6MTYxOTU2NzY1NywiaXNzIjoiZHJvcGJhc2UuaW8iLCJzdWIiOiJvQ3NQODJqOTY2dTgyOXpxN0xoVU00In0.XJF-YbxpBrYQd67epG2LPFHgTn18N4rdmHZWBgwzIus"
+
+    var = Database(TOKEN, REST_KEY)
+
+    # var.upload_to_db_from_file("colortests copy")
+
+    query = '?color=eq.gaurab'
+    default_query = "null"
+    table_to_query = "color1"
+
+    var.rest_api(table_to_query, default_query)
+
+
+
